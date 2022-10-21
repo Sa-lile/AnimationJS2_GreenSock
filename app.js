8b05bd3862d58ecc5fc09b9f132d67fdb0259e0c
Sa-lile <sachiyo.sable@gmail.com>
2022-10-21 10:45:02 +0200

    Fini animation JSAP partie1

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-// Fichier Js de départ
+window.onload = function(){
+
+    let tl1 = new TimelineMax();
+
+    tl1
+    .from('.sphere', 0.8, {delay: 0.1, scale: 0, top:"50%", left:"50%", opacity: 0, ease: Back.easeOut.config(1.7)})
+
+    .to('.intro-container', 1, {top: 80, left: 82, right: 82, bottom: 80}, '+0.15')
+
+    .to('.sphere', 1, {left:'75%', ease: Back.easeOut.config(2.7)})
+}
